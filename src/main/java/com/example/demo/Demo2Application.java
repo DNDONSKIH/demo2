6344dfb4f451feedc5ca86cced12407a78dd58db
Dmitriy Donskikh <dndonskih@gmail.com>
2022-04-05 00:58:30 +0300

    task2

--- src/main/java/com/example/demo/Demo2Application.java
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -3,10 +3,24 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Random;
+
 @SpringBootApplication
 public class Demo2Application {
 
     public static void main(String[] args) {
+
+
+        Random r = new Random();
+        int [] intArray = new int[100000];
+
+        for (int i = 0; i < intArray.length; i++) {
+            intArray[i] = r.nextInt(100001);
+        }
+
+
+
+
         SpringApplication.run(Demo2Application.class, args);
     }
 

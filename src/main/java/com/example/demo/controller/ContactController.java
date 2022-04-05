62053e3c473def655ef627e29ca12e3b1f792232
Dmitriy Donskikh <dndonskih@gmail.com>
2022-04-05 22:24:07 +0300

    task2 fix

--- src/main/java/com/example/demo/controller/ContactController.java
+++ src/main/java/com/example/demo/controller/ContactController.java
@@ -9,11 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import javax.servlet.http.HttpServletRequest;
 import java.util.*;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/contacts")
 public class ContactController {
 
     @Autowired
@@ -22,8 +21,8 @@ public class ContactController {
     @Autowired
     private PhoneNumberRepository phoneNumberRepository;
 
-    @GetMapping("/contacts")
-    public String getContacts(Model model) {
+    @GetMapping
+    public String getAllContacts(Model model) {
         Iterable<Contact> contactIterable = contactRepository.findAll();
         var contacts = new ArrayList<Contact>();
         contactIterable.forEach(contact -> { contacts.add(contact); });
@@ -31,111 +30,8 @@ public class ContactController {
         return "contacts";
     }
 
-    @PostMapping("/contacts")
-    public String addNewContact(@RequestParam("surname") String surname,
-                                @RequestParam("middlename") String middlename,
-                                @RequestParam("lastname") String lastname,
-                                @RequestParam("birthday") String birthday,
-                                Model model) {
-
-        String [] dateSubstring = birthday.split("-");
-        int dateYearNum, dateMonthNum, dateDayNum;
-        try {
-            dateYearNum = Integer.parseInt(dateSubstring[0]);
-            dateMonthNum = Integer.parseInt(dateSubstring[1]);
-            dateDayNum = Integer.parseInt(dateSubstring[2]);
-        }
-        catch (Exception ex){
-            dateYearNum = 2000;
-            dateMonthNum = 1;
-            dateDayNum = 1;
-        }
-
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.YEAR, dateYearNum);
-        cal.set(Calendar.MONTH, dateMonthNum);
-        cal.set(Calendar.DAY_OF_MONTH, dateDayNum);
-        Date date = cal.getTime();
-
-        var newContact = new Contact();
-        var numList = new ArrayList<PhoneNumber>();
-        newContact.setSurname(surname);
-        newContact.setMiddleName(middlename);
-        newContact.setLastName(lastname);
-        newContact.setBirthday(date);
-
-        newContact.setPhoneNumberList( numList );
-        contactRepository.save(newContact);
-
-        Iterable<Contact> contactIterable = contactRepository.findAll();
-        var contacts = new ArrayList<Contact>();
-        contactIterable.forEach(contact -> { contacts.add(contact); });
-        model.addAttribute("contacts", contacts);
-
-        return "contacts";
-    }
-
-    @PostMapping("/contacts/{id}")
-    public String addPhoneNumberById(@PathVariable Integer id,
-                                     @RequestParam(name = "value", required = false, defaultValue = "") String requestValue,
-                                     @RequestParam(name = "phone-type", required = false, defaultValue = "cellphone") String requestPhoneType,
-                                     Model model) {
-        Contact contact = contactRepository.findById(id).orElse(new Contact());
-
-        PhoneType phoneType;
-        switch (requestPhoneType) {
-            case "home":
-                phoneType = PhoneType.HOME;
-                break;
-            case "cellphone":
-                phoneType = PhoneType.CELLPHONE;
-                break;
-            default:
-                phoneType = PhoneType.WORK;
-                break;
-        }
-
-        var newPhoneNumber = new PhoneNumber();
-        newPhoneNumber.setValue(requestValue);
-        newPhoneNumber.setType(phoneType);
-        newPhoneNumber.setContact(contact);
-
-        var numlist = contact.getPhoneNumberList();
-        numlist.add(newPhoneNumber);
-        contact.setPhoneNumberList( numlist );
-        contactRepository.save(contact);
-
-        model.addAttribute("contact", contact);
-        return "redirect:/contacts/" + id.toString();
-    }
-
-    @PostMapping("/contacts/del/{id}")
-    public String deleteContactById(@PathVariable Integer id, Model model) {
-        Contact deletedContact = contactRepository.findById(id).orElse(new Contact());
-        contactRepository.delete(deletedContact);
-
-        Iterable<Contact> contactIterable = contactRepository.findAll();
-        var contacts = new ArrayList<Contact>();
-        contactIterable.forEach(contact -> { contacts.add(contact); });
-        model.addAttribute("contacts", contacts);
-        return "redirect:/contacts";
-    }
-
-    @PostMapping("/contacts/{id}/del/{phoneid}")
-    public String addPhoneNumberById(@PathVariable Integer id,
-                                     @PathVariable Integer phoneid,
-                                     Model model) {
-
-        PhoneNumber deletedPhoneNumber = phoneNumberRepository.findById(phoneid).orElse(new PhoneNumber());
-        phoneNumberRepository.delete(deletedPhoneNumber);
-
-        Contact contact = contactRepository.findById(id).orElse(new Contact());
-        model.addAttribute("contact", contact);
-        return "redirect:/contacts/" + id.toString();
-    }
-
-    @GetMapping("/contacts/{id}")
-    public String getContacById(@PathVariable Integer id, Model model) {
+    @GetMapping("/{id}")
+    public String getContactById(@PathVariable Integer id, Model model) {
         Contact contact = contactRepository.findById(id).orElse(new Contact());
         model.addAttribute("contact", contact);
         return "contact";
@@ -155,13 +51,85 @@ public class ContactController {
         return "filtered-by-phone-number";
     }
 
+    @PostMapping
+    public String addNewContact(@RequestParam("surname") String surname,
+                                @RequestParam("middlename") String middlename,
+                                @RequestParam("lastname") String lastname,
+                                @RequestParam("birthday") String birthday) {
+
+        String [] dateSubstring = birthday.split("-");
+        int dateYearNum = 2000;
+        int dateMonthNum = 1;
+        int dateDayNum = 1;
+        try {
+            dateYearNum = Integer.parseInt(dateSubstring[0]);
+            dateMonthNum = Integer.parseInt(dateSubstring[1]);
+            dateDayNum = Integer.parseInt(dateSubstring[2]);
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, dateYearNum);
+        cal.set(Calendar.MONTH, dateMonthNum);
+        cal.set(Calendar.DAY_OF_MONTH, dateDayNum);
+        Date date = cal.getTime();
+
+        var newContact = new Contact();
+        var numList = new ArrayList<PhoneNumber>();
+        newContact.setSurname(surname);
+        newContact.setMiddleName(middlename);
+        newContact.setLastName(lastname);
+        newContact.setBirthday(date);
+
+        newContact.setPhoneNumberList( numList );
+        contactRepository.save(newContact);
+        return "redirect:/contacts/";
+    }
+
+    @PostMapping("/{id}")
+    public String addPhoneNumberById(@PathVariable Integer id,
+                                     @RequestParam(name = "value", required = false, defaultValue = "") String requestValue,
+                                     @RequestParam(name = "phone-type", required = false, defaultValue = "cellphone") String requestPhoneType) {
+        Contact contact = contactRepository.findById(id).orElse(new Contact());
+
+        PhoneType phoneType = switch (requestPhoneType) {
+            case "home" -> PhoneType.HOME;
+            case "cellphone" -> PhoneType.CELLPHONE;
+            default -> PhoneType.WORK;
+        };
+
+        var newPhoneNumber = new PhoneNumber();
+        newPhoneNumber.setValue(requestValue);
+        newPhoneNumber.setType(phoneType);
+        newPhoneNumber.setContact(contact);
+
+        var numList = contact.getPhoneNumberList();
+        numList.add(newPhoneNumber);
+        contact.setPhoneNumberList( numList );
+        contactRepository.save(contact);
+        return "redirect:/contacts/" + id.toString();
+    }
+
+    @PostMapping("/del/{id}")
+    public String deleteContactById(@PathVariable Integer id) {
+        Contact deletedContact = contactRepository.findById(id).orElse(new Contact());
+        contactRepository.delete(deletedContact);
+        return "redirect:/contacts";
+    }
+
+    @PostMapping("/{id}/del/{phoneid}")
+    public String deletePhoneNumberById(@PathVariable Integer id,
+                                     @PathVariable Integer phoneid) {
+        PhoneNumber deletedPhoneNumber = phoneNumberRepository.findById(phoneid).orElse(new PhoneNumber());
+        phoneNumberRepository.delete(deletedPhoneNumber);
+        return "redirect:/contacts/" + id.toString();
+    }
+
 //    @GetMapping("/find-by-phone-number") //другой способ получить параметры get апроса
 //    public @ResponseBody List<Contact> getContactByPhoneNumber(HttpServletRequest request) {
 //        String number = request.getParameter("number");
 //        return contactRepository.findByPhoneNumberList_Value(number);
 //    }
-
-//    @GetMapping("/contact-list")
-//    public @ResponseBody Iterable<Contact> getAllContacts() { return contactRepository.findAll(); }
-
 }

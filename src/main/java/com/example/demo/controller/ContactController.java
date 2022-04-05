33b78a14100729e1b69877df8b8a9d2a6bc2ef9c
Dmitriy Donskikh <dndonskih@gmail.com>
2022-04-06 02:18:24 +0300

    task2 add patch mapping

--- src/main/java/com/example/demo/controller/ContactController.java
+++ src/main/java/com/example/demo/controller/ContactController.java
@@ -37,6 +37,15 @@ public class ContactController {
         return "contact";
     }
 
+
+    @GetMapping("/{id}/edit")
+    public String editContactById(@PathVariable Integer id, Model model) {
+        Contact contact = contactRepository.findById(id).orElse(new Contact());
+        model.addAttribute("contact", contact);
+        return "contact-edit";
+    }
+
+
     @GetMapping("/filtered-by-surname")
     public String getContactByName(@RequestParam("surname") String surname, Model model) {
         List<Contact> contacts = contactRepository.findBySurname(surname);
@@ -112,20 +121,64 @@ public class ContactController {
         return "redirect:/contacts/" + id.toString();
     }
 
-    @PostMapping("/del/{id}")
-    public String deleteContactById(@PathVariable Integer id) {
-        Contact deletedContact = contactRepository.findById(id).orElse(new Contact());
+    @DeleteMapping
+    public String deleteContactById(@RequestParam("contactId") String contactId) {
+        int currentContactId = Integer.parseInt(contactId);
+        Contact deletedContact = contactRepository
+                .findById(currentContactId).orElse(new Contact());
         contactRepository.delete(deletedContact);
         return "redirect:/contacts";
     }
 
-    @PostMapping("/{id}/del/{phoneid}")
-    public String deletePhoneNumberById(@PathVariable Integer id,
-                                     @PathVariable Integer phoneid) {
-        PhoneNumber deletedPhoneNumber = phoneNumberRepository.findById(phoneid).orElse(new PhoneNumber());
+    @DeleteMapping("/{id}")
+    public String deletePhoneNumberById(@RequestParam("phoneId") String phoneId,
+                                        @PathVariable Integer id) {
+        int currentPhoneId = Integer.parseInt(phoneId);
+        PhoneNumber deletedPhoneNumber = phoneNumberRepository
+                .findById(currentPhoneId).orElse(new PhoneNumber());
         phoneNumberRepository.delete(deletedPhoneNumber);
         return "redirect:/contacts/" + id.toString();
     }
+
+    @PatchMapping("/{id}")
+    public String deletePhoneNumberById(@RequestParam("surname") String surname,
+                                        @RequestParam("middlename") String middlename,
+                                        @RequestParam("lastname") String lastname,
+                                        @RequestParam("birthday") String birthday,
+                                        @PathVariable Integer id) {
+
+        String [] dateSubstring = birthday.split("-");
+        int dateYearNum = 2000;
+        int dateMonthNum = 1;
+        int dateDayNum = 1;
+        try {
+            dateYearNum = Integer.parseInt(dateSubstring[0]);
+            dateMonthNum = Integer.parseInt(dateSubstring[1]);
+            dateDayNum = Integer.parseInt(dateSubstring[2]);
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, dateYearNum);
+        cal.set(Calendar.MONTH, dateMonthNum);
+        cal.set(Calendar.DAY_OF_MONTH, dateDayNum);
+        Date date = cal.getTime();
+
+        Contact patchedContact = contactRepository
+                .findById(id).orElse(new Contact());
+
+        patchedContact.setSurname(surname);
+        patchedContact.setMiddleName(middlename);
+        patchedContact.setLastName(lastname);
+        patchedContact.setBirthday(date);
+
+        contactRepository.save(patchedContact);
+
+        return "redirect:/contacts/" + id.toString();
+    }
+
 
 //    @GetMapping("/find-by-phone-number") //другой способ получить параметры get апроса
 //    public @ResponseBody List<Contact> getContactByPhoneNumber(HttpServletRequest request) {

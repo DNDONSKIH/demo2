b55d8d1c41d8c4457f7254aad05696abe755330b
Dmitriy Donskikh <dndonskih@gmail.com>
2022-04-08 01:26:03 +0300

    fix2

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,25 +1,25 @@
-$(function(){
-    //Show add contact form
-    $('#show-add-contact-form').click(function(){
-        $('#create-contact-form').css('display', 'flex');
-    });
-
-    //Closing add contact form
-    $('#create-contact-form').click(function(event){
-        if(event.target === this) {
-            $(this).css('display', 'none');
-        }
-    });
-
-    //Show find contact form
-    $('#show-find-contact-form').click(function(){
-        $('#find-contact-form').css('display', 'flex');
-    });
-
-    //Closing find contact form
-    $('#find-contact-form').click(function(event){
-        if(event.target === this) {
-            $(this).css('display', 'none');
-        }
-    });
-});
+// $(function(){
+//     //Show add contact form
+//     $('#show-add-contact-form').click(function(){
+//         $('#create-contact-form').css('display', 'flex');
+//     });
+//
+//     //Closing add contact form
+//     $('#create-contact-form').click(function(event){
+//         if(event.target === this) {
+//             $(this).css('display', 'none');
+//         }
+//     });
+//
+//     //Show find contact form
+//     $('#show-find-contact-form').click(function(){
+//         $('#find-contact-form').css('display', 'flex');
+//     });
+//
+//     //Closing find contact form
+//     $('#find-contact-form').click(function(event){
+//         if(event.target === this) {
+//             $(this).css('display', 'none');
+//         }
+//     });
+// });

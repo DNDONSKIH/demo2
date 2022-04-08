aa5e40e9553a597da2faaf28af97ec3d1c916253
Dmitriy Donskikh <dndonskih@gmail.com>
2022-04-08 22:01:59 +0300

    fix

--- src/main/java/com/example/demo/components/DateStringToDateConverter.java
+++ src/main/java/com/example/demo/components/DateStringToDateConverter.java
@@ -1,34 +1,21 @@
 package com.example.demo.components;
-
-import lombok.Data;
 import org.springframework.stereotype.Component;
-
-import java.util.Calendar;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
-
 
 @Component
 public class DateStringToDateConverter {
 
     public Date getDateFromDateString(String dateString){
-
-        String [] dateSubstring = dateString.split("-");
-        int dateYearNum = 2000;
-        int dateMonthNum = 1;
-        int dateDayNum = 1;
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date parsedDate = new Date();
         try {
-            dateYearNum = Integer.parseInt(dateSubstring[0]);
-            dateMonthNum = Integer.parseInt(dateSubstring[1]);
-            dateDayNum = Integer.parseInt(dateSubstring[2]);
+            parsedDate = format.parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
-        catch (Exception ex){
-            ex.printStackTrace();
-        }
-
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.YEAR, dateYearNum);
-        cal.set(Calendar.MONTH, (dateMonthNum-1));
-        cal.set(Calendar.DAY_OF_MONTH, dateDayNum);
-        return cal.getTime();
+        return parsedDate;
     }
+
 }
--- src/main/resources/static/js/mybestscript.js
+++ src/main/resources/static/js/mybestscript.js
@@ -2,6 +2,8 @@ const addContactButton = document.querySelector("#show-add-contact-form");
 const createContactForm = document.getElementById('create-contact-form')
 const findContactButton = document.querySelector("#show-find-contact-form");
 const findContactForm = document.getElementById('find-contact-form')
+const formWithData = document.querySelector('#add-contact-form-ajax')
+const fetchTestButton = document.querySelector("#add-contact-form-ajax");
 
 addContactButton.addEventListener('click',function() {
     createContactForm.style.display = "flex";
@@ -19,34 +21,9 @@ findContactForm.addEventListener('click',function(event){
     if(event.target === this) {this.style.display = "none";}
 })
 
-
-
-
-
-// const fetchTestButton = document.querySelector("#fetch-post-button");
-//
-// fetchTestButton.addEventListener('click',function(event){
-//     event.preventDefault();
-//     testPost();
-// })
-
-
-const fetchTestButton = document.querySelector("#add-contact-form-ajax");
-
-fetchTestButton.addEventListener('submit',function(event){
-    event.preventDefault();
-    testPost();
-})
-
-//const formWithData = document.getElementById('add-contact-form-ajax')
-const formWithData = document.querySelector('#add-contact-form-ajax')
-
 const testPost = async () => {
-
     const newContactForm = new FormData(formWithData);
-    for (let [key, value] of newContactForm.entries()) {
-        console.log(key, value);
-    }
+    // for (let [key, value] of newContactForm.entries()) {  console.log(key, value); }
 
     try {
         const response = await fetch('/contacts/addnew', {
@@ -54,14 +31,16 @@ const testPost = async () => {
             body: newContactForm
         });
         const result = await response.json();
-        console.log('Успех:', JSON.stringify(result));
+        console.log('SUCCESS:', JSON.stringify(result));
     } catch (error) {
-        console.error('Ошибка:', error);
+        console.error('ERROR:', error);
     }
 }
 
-
-
+fetchTestButton.addEventListener('submit',function(event){
+    event.preventDefault();
+    testPost();
+})
 
 
 

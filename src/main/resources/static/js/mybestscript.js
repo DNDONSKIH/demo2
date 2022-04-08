9645e56bad05722bbac7af56008a676908b0f781
dnd <dndonskih@gmail.com>
2022-04-08 09:35:05 +0300

    fix3

--- src/main/resources/static/js/mybestscript.js
+++ src/main/resources/static/js/mybestscript.js
@@ -1,25 +1,21 @@
 const addContactButton = document.querySelector("#show-add-contact-form");
 const createContactForm = document.getElementById('create-contact-form')
+const findContactButton = document.querySelector("#show-find-contact-form");
+const findContactForm = document.getElementById('find-contact-form')
 
 addContactButton.addEventListener('click',function() {
     createContactForm.style.display = "flex";
 })
 
 createContactForm.addEventListener('click',function(event){
-    if(event.target === this) { createContactForm.style.display = "none"; }
+    if(event.target === this) { this.style.display = "none"; }
 })
-
-const findContactButton = document.querySelector("#show-find-contact-form");
-const findContactForm = document.getElementById('find-contact-form')
 
 findContactButton.addEventListener('click',function() {
     findContactForm.style.display = "flex";
 })
 
 findContactForm.addEventListener('click',function(event){
-    if(event.target === this) {
-        findContactForm.style.display = "none";
-        //this.style.display = "none" //работает, но почему this в этом контексте указывает на div!???
-    }
+    if(event.target === this) {this.style.display = "none";}
 })
 
